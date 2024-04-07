f224bf7f3378cada818518fce097c415c9785df4
Serverket <serverket@mailfence.com>
2024-04-07 16:58:57 -0400

    Both local and Vercel routing.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const cors_anywhere = require('cors-anywhere');
+const http = require('http');
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+const port = process.env.PORT || 3000;
 
 cors_anywhere.createServer({
-    originWhitelist: ['https://uratest.site'], // Allow requests from 'https://uratest.site'
+    originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(function () {
-    console.log('Running CORS Anywhere');
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
