8b1fcbc59edcef70a8c607694f7d1ffc2ede9e13
Serverket <serverket@mailfence.com>
2024-04-07 14:35:46 -0400

    Vercel config.

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ cors_anywhere.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
+}).listen(port, host, function () {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
